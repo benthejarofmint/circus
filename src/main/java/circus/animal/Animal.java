045e013623792478d3bd5d57a9f9a8d0eefaa4a5
ben <jwben03@gmail.com>
2026-02-13 17:38:42 +0800

    Add A-jAr manifest

--- src/main/java/circus/animal/Animal.java
+++ src/main/java/circus/animal/Animal.java
@@ -6,7 +6,6 @@ import java.util.Comparator;
 
 public abstract class Animal implements Asset {
     public abstract String speak();
-<<<<<<< HEAD
 
     public static Comparator<Animal> AnimalNameComparator = new Comparator<Animal>() {
         @Override
@@ -14,7 +13,5 @@ public abstract class Animal implements Asset {
             return animal1.name.compareToIgnoreCase(animal2.name);
         }
     };
-=======
     public String name;
->>>>>>> 5c03dba8a6f0a2e8dbc62d35506b50d9df6ced76
 }
